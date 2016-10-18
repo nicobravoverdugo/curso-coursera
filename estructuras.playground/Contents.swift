70d546da495b53a466b457be6430bf99aa4e2196
Nicolás Bravo Verdugo <nicobravoverdugo@gmail.com>
2016-10-18 15:32:42 -0300

    Estructuras 1.0

--- estructuras.playground/Contents.swift
+++ estructuras.playground/Contents.swift
@@ -6,8 +6,21 @@ struct Automovil
 {
     var marca : String
     var precio : Double
+
+    init(marca : String, precio : Double) {
+        self.marca = marca
+        self.precio = precio
+    }
+    
+    func obtenerDescripcion( ) -> String
+    {
+        return "marca: \(marca)\tprecio: \(precio)"
+    }
 }
 
 var auto = Automovil(marca: "MarcaAuto", precio: 19800.80)
 auto.marca
+auto.precio = 9890
 auto.precio
+
+auto.obtenerDescripcion()
