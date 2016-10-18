79b417dd67b6e2033cc0777bc2d43993a354f043
Nicolás Bravo Verdugo <nicobravoverdugo@gmail.com>
2016-10-18 15:20:29 -0300

    Estructuras Ver 1.0

---
+++ estructuras.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Estructuras
+
+import UIKit
+
+struct Automovil
+{
+    var marca : String
+    var precio : Double
+}
+
+var auto = Automovil(marca: "MarcaAuto", precio: 19800.80)
+auto.marca
+auto.precio
