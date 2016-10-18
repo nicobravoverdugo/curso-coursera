4a7696e887ba9b6574b7f84499fd5cf29cb06e31
Nicolás Bravo Verdugo <nicobravoverdugo@gmail.com>
2016-10-18 11:18:17 -0300

    Opcionales Ver 1.1

--- opcionales.playground/Contents.swift
+++ opcionales.playground/Contents.swift
@@ -12,6 +12,35 @@ if numero != nil
 {
     let numeroString : String = String(numero!) // Accesa el numero !
     print(numeroString)
+}
+
+func profesores( id: String) -> String?
+{
+    let diccionarioDeProfesores = ["007":"David",
+        "008":"Martin",
+        "009":"Rafa",
+        "010":"Víctor"]
+    let nombre : String? = diccionarioDeProfesores[id]!
     
-    
+    return nombre
+}
+
+if let nombre = profesores(id: "009")
+{
+    nombre
+}
+else
+{
+    print("No hay profesor")
+}
+
+let nombreDos = profesores(id: "010")
+
+if nombreDos != nil
+{
+    nombreDos
+}
+else
+{
+    print("No hay profesor")
 }
